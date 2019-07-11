b047eeacc4b00a3e33a28f1eef98bad8881a16f2
larsthrasher <thrasherlars@gmail.com>
2019-07-11 18:01:36 -0400

    other routes added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,40 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     render json: @post, status: :ok
   end
+  def new
+   @post = Post.new
+   render json: @post, status: :ok
+ end
+
+ def create
+    @post = Post.create(post_params)
+    # redirect_to @posts
+    render json: @post, status: :ok
+
+ end
+ def edit
+   @post = Post.find(params[:id])
+   render json: @post, status: :ok
+ end
+
+ def update
+    @post = Post.find(params[:id])
+   if @post.update(post_params)
+     redirect_to posts_path
+   else
+     render :edit
+   end
+ end
+
+ def destroy
+   @post = Post.find(params[:id])
+   @post.delete
+   # redirect_to posts_path
+ end
+
+ private
+
+ def post_params
+   params.permit(:contact, :title, :image, :description)
+ end
 end
