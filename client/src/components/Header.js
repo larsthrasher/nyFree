70e943ce7d617f48ce5647864fd57956cd0fe9ac
larsthrasher <thrasherlars@gmail.com>
2019-07-18 16:44:22 -0400

    header added

---
+++ client/src/components/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import axios from "axios";
+import CreatePost from "./CreatePost.js";
+import "../App.css";
+
+class Header extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+      <div className="header">
+        // <h1>
+        //   nyFree
+        //   <span className="give">&nbsp;....hey New York, give it away!</span>
+        // </h1>
+        <div className="form">
+          <CreatePost onNewPostCreated = {this.props.onNewPostCreated} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
