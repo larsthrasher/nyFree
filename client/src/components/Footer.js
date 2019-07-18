73bc62763ace120eb99e54cb0d3c803c33f87ecb
larsthrasher <thrasherlars@gmail.com>
2019-07-18 16:45:53 -0400

    footer added

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from "react";
+
+const Footer = () => {
+   return(
+     <div className = "footer">
+        <h5>Lars Thrasher 2019, https://github.com/larsthrasher/nyFree</h5>
+     </div>
+   );
+ }
+
+
+export default Footer;
