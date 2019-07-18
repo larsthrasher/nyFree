4db110ccc7bfa00a9c41c41a8d8a43efd620a823
larsthrasher <thrasherlars@gmail.com>
2019-07-18 16:39:44 -0400

    create form component up

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -14,5 +14,86 @@ class CreatePost extends Component {
       descriptionText: ""
     }
   }
-  
+  handleChangeContact = e => {
+    this.setState({ contactText: e.target.value })
+  };
+
+  handleChangeTitle = e => {
+    this.setState({ titleText: e.target.value })
+  };
+
+  handleChangeImage = e => {
+    this.setState({ imageText: e.target.value })
+  };
+
+  handleChangeDescription = e => {
+    this.setState({ descriptionText: e.target.value })
+  };
+
+  clearForm = () => {
+    this.setState({
+      contactText: "",
+      titleText: "",
+      imageText: "",
+      descriptionText: ""
+    })
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    if (
+      !this.state.contactText.length ||
+      !this.state.titleText.length ||
+      !this.state.imageText.length ||
+      !this.state.descriptionText.length
+    ) {
+      return alert("Enter some stuff");
+    }
+    const newItem = {
+      contact: this.state.contactText,
+      title: this.state.titleText,
+      image: this.state.imageText,
+      description: this.state.descriptionText
+    }
+    const resp = await axios.post(`${URL}/posts`, newItem);
+    await this.props.onNewPostCreated();
+    this.clearForm();
+  }
+
+  render(){
+    const { contactText, titleText, imageText, descriptionText } = this.state;
+    return (
+      <div className="CreatePost">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            id="contact"
+            onChange={this.handleChangeContact}
+            value={contactText}
+            placeholder="Contact Info"
+          />
+          <input
+            id="title"
+            onChange={this.handleChangeTitle}
+            value={titleText}
+            placeholder="Item Title"
+          />
+          <input
+            id="image"
+            onChange={this.handleChangeImage}
+            value={imageText}
+            placeholder="Image Address"
+          />
+          <input
+            id="description"
+            onChange={this.handleChangeDescription}
+            value={descriptionText}
+            placeholder="Item Description"
+          />
+          <input className="submit" type="submit" value="Add Post"/>
+        </form>
+      </div>
+    )
+  }
 }
+
+export default CreatePost;
